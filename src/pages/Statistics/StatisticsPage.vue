121a3059aa0a8ad1ffe3748d826a3a860703ec41
de-lolec <matvey20158@mail.ru>
2025-01-24 20:42:29 -0300

    dev: transfer part statistics to nutrient circular progress component

--- src/pages/Statistics/StatisticsPage.vue
+++ src/pages/Statistics/StatisticsPage.vue
@@ -21,110 +21,26 @@
         <q-card-section class="col-12" v-if="!isLoading">
           <div class="text-body1 text-bold">Statistics for day:</div>
           <div class="row justify-center q-gutter-sm">
-            <div class="flex justify-center column items-center">
-              <q-circular-progress
-                show-value
-                class="text-orange-8 q-ma-xs"
-                :value="dailyCalories"
-                size="65px"
-                :max="user.maxNutrients.calories"
-                color="orange-8"
-                track-color="grey-3"
-                rounded
-                :angle="180"
-              >
-                <div class="column">
-                  <div
-                    class="text-grey-8 q-ma-none q-ml-sm"
-                    style="font-size: 12px"
-                  >
-                    {{ dailyCalories }}
-                  </div>
-                  <div class="text-grey-8 q-ma-none" style="font-size: 12px">
-                    / {{ user.maxNutrients.calories ?? '?' }}
-                  </div>
-                </div>
-              </q-circular-progress>
-              <div class="text-grey-8" style="font-size: 12px">Calories</div>
-            </div>
-            <div class="flex justify-center column items-center">
-              <q-circular-progress
-                show-value
-                class="text-orange-8 q-ma-xs"
-                :value="dailyProteins"
-                size="65px"
-                :max="user.maxNutrients.proteins"
-                color="orange-8"
-                track-color="grey-3"
-                rounded
-                :angle="180"
-              >
-                <div class="column">
-                  <div
-                    class="text-grey-8 q-ma-none q-ml-sm"
-                    style="font-size: 12px"
-                  >
-                    {{ dailyProteins }}
-                  </div>
-                  <div class="text-grey-8 q-ma-none" style="font-size: 12px">
-                    / {{ user.maxNutrients.proteins ?? '?' }}
-                  </div>
-                </div>
-              </q-circular-progress>
-              <div class="text-grey-8" style="font-size: 12px">Proteins</div>
-            </div>
-            <div class="flex justify-center column items-center">
-              <q-circular-progress
-                show-value
-                class="text-orange-8 q-ma-xs"
-                :value="dailyCarbs"
-                size="65px"
-                :max="user.maxNutrients.carbs"
-                color="orange-8"
-                track-color="grey-3"
-                rounded
-                :angle="180"
-              >
-                <div class="column">
-                  <div
-                    class="text-grey-8 q-ma-none q-ml-sm"
-                    style="font-size: 12px"
-                  >
-                    {{ dailyCarbs }}
-                  </div>
-                  <div class="text-grey-8 q-ma-none" style="font-size: 12px">
-                    / {{ user.maxNutrients.carbs ?? '?' }}
-                  </div>
-                </div>
-              </q-circular-progress>
-              <div class="text-grey-8" style="font-size: 12px">Carbs</div>
-            </div>
-            <div class="flex justify-center column items-center">
-              <q-circular-progress
-                show-value
-                class="text-orange-8 q-ma-xs"
-                :value="dailyFats"
-                size="65px"
-                :max="user.maxNutrients.fats"
-                color="orange-8"
-                track-color="grey-3"
-                rounded
-                :angle="180"
-              >
-                <div class="column">
-                  <div
-                    class="text-grey-8 q-ma-none q-ml-sm"
-                    style="font-size: 12px"
-                  >
-                    {{ dailyFats }}
-                  </div>
-                  <div class="text-grey-8 q-ma-none" style="font-size: 12px">
-                    / {{ user.maxNutrients.fats ?? '?' }}
-                  </div>
-                </div>
-              </q-circular-progress>
-              <div class="text-grey-8" style="font-size: 12px">Fats</div>
-            </div>
+            <NutrientCircularProgress
+              :value="dailyCalories"
+              :max="user.maxNutrients.calories"
+              label="Calories"
+            />
+            <NutrientCircularProgress
+              :value="dailyProteins"
+              :max="user.maxNutrients.proteins"
+              label="Proteins"
+            />
+            <NutrientCircularProgress
+              :value="dailyCarbs"
+              :max="user.maxNutrients.carbs"
+              label="Carbs"
+            />
+            <NutrientCircularProgress
+              :value="dailyFats"
+              :max="user.maxNutrients.fats"
+              label="Fats"
+            />
           </div>
         </q-card-section>
         <q-card-section class="self-center" v-else>
@@ -136,6 +52,7 @@
           />
         </q-card-section>
         <q-separator inset />
+        <!-- Statistics for week -->
         <q-card-section class="col-12" v-if="!isLoading">
           <div class="text-body1 text-bold q-mb-sm">Statistics for week:</div>
 
@@ -162,8 +79,9 @@ import { computed, onMounted, ref } from 'vue';
 import { useDailyMealStore } from '../../stores/dailyMealStore';
 import { useUserStore } from '../../stores/userStore';
 import { useStatisticsStore } from '../../stores/statisticsStore';
-import ChartLineForPeriodDate from 'src/components/Statistics/ChartLineForPeriodDate.vue';
 import { formatToLocal } from '../../utils/Formatters/dateFormatter.ts';
+import ChartLineForPeriodDate from 'src/components/Statistics/ChartLineForPeriodDate.vue';
+import NutrientCircularProgress from 'src/components/Statistics/NutrientCircularProgress.vue';
 
 const dailyMealStore = useDailyMealStore();
 const statisticsStore = useStatisticsStore();
