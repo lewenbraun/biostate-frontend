97fc263986b97dbe3ce244a4cadbc4bcfbe04a2c
de-lolec <matvey20158@mail.ru>
2025-03-24 02:45:42 -0400

    feat: renamed project name to header and nginx conf

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -6,7 +6,7 @@
           @click="$router.push({ name: 'main' })"
           style="cursor: pointer"
         >
-          What-to-eat-a
+          BioState
         </q-toolbar-title>
         <div class="row q-gutter-xs">
           <q-btn flat :to="{ name: 'login' }">Sign in</q-btn>
